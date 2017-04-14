26658d65700a1b315dd838995e29b777a73a6fe7
Esteban Bustos F <ngprodev@gmail.com>
2017-04-14 16:43:57 -0300

    input component

---
+++ projectOne/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { InputComponent } from './input/input.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+
+  votos = [
+  	{title: 'opcion 1'},
+  	{title: 'opcion 2'},
+  	{title: 'opcion 3'},
+  	{title: 'opcion 4'}  	
+  ]
+}
