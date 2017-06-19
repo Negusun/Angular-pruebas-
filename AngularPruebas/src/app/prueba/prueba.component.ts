588b0470fd0cc760426f1e84bd16926630455043
Negusun <ngprodev@gmail.com>
2017-06-19 16:31:59 -0400

    orden

--- AngularPruebas/src/app/prueba/prueba.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'prueba-main',
-  templateUrl: './prueba.component.html'
-})
-export class PruebaComponent {
-  title = 'Mi primer componente!';
-}
