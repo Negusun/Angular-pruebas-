500ec1243e0d88fe2eeea9edfe64093a0eb1782c
Negusun <ngprodev@gmail.com>
2017-07-27 18:28:46 -0400

    up form

--- formulario/src/app/components/data/data.component.ts
+++ formulario/src/app/components/data/data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
+  form:FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      'nombre': new FormControl('', [
+                                      Validators.required,
+                                      Validators.minLength(3),
+                                      Validators.pattern('[A-Za-z]*'),
+                                      Validators.maxLength(15)
+                                    ]),
+      'apellido': new FormControl('', [
+                                      Validators.required,
+                                      Validators.minLength(3),
+                                      Validators.pattern('[A-Za-z]*'),
+                                      Validators.maxLength(15)
+                                    ]),
+      'email': new FormControl('', [
+                                      Validators.required,
+                                      Validators.pattern('^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.].[a-z]{2,5}')
+                                    ])
+    });
+  }
+
+  guardar()
+  {
+    console.log(this.form.value);
   }
 
 }
