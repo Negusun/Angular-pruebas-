e39b98783225edfd4ce32477c91aa53463480920
Negusun <ngprodev@gmail.com>
2017-07-28 18:35:19 -0400

    validacion personalizada

--- formulario/src/app/components/data/data.component.ts
+++ formulario/src/app/components/data/data.component.ts
@@ -15,6 +15,9 @@ export class DataComponent implements OnInit {
       nombre:'Esteban',
       apellido:'Bustos'
     },
+    nomusuario:'',
+    password1:'',
+    password2:'',
     email:'ngprodev@gmail.com',
     skills:['Ver Series']
   }
@@ -37,15 +40,31 @@ export class DataComponent implements OnInit {
                                         Validators.maxLength(15)
                                       ])
       }),
+      'nomusuario': new FormControl(''),
+      'password1': new FormControl('', Validators.required),
+      'password2': new FormControl('', Validators.required,),
       'email': new FormControl('', [
                                       Validators.required,
                                       Validators.pattern('^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.].[a-z]{2,5}')
                                     ]),
-      'skills': new FormArray([
-                  new FormControl('', Validators.required)
-      ])
+      'skills': new FormArray([])
     });
-    this.form.setValue(this.usuario);
+    this.form.controls['password2'].setValidators(
+      [
+        Validators.required,
+        this.passinvalid.bind(this.form)
+      ]
+    );
+  }
+
+
+
+  passinvalid( ctr:FormControl ):any {
+    let form: any = this;
+    if(ctr.value!== form.get('password1').value) {
+      return { passinvalid : true }
+    }
+    return true;
   }
 
   newSkill() {
@@ -59,7 +78,7 @@ export class DataComponent implements OnInit {
   }
 
   guardar()  {
-    console.log(this.form.value);
+    console.log(this.form);
   }
 
 }
