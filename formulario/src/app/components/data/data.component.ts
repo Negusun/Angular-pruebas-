cd42cb848c6847ee00d0581985e19f0150a3d344
Negusun <ngprodev@gmail.com>
2017-07-29 14:52:36 -0400

    validaciones asincronas y subscribirse a cambios en el form

--- formulario/src/app/components/data/data.component.ts
+++ formulario/src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-data',
@@ -40,7 +41,7 @@ export class DataComponent implements OnInit {
                                         Validators.maxLength(15)
                                       ])
       }),
-      'nomusuario': new FormControl(''),
+      'nomusuario': new FormControl('', Validators.required, this.userexist),
       'password1': new FormControl('', Validators.required),
       'password2': new FormControl('', Validators.required,),
       'email': new FormControl('', [
@@ -55,6 +56,13 @@ export class DataComponent implements OnInit {
         this.passinvalid.bind(this.form)
       ]
     );
+
+    this.form.valueChanges
+      .subscribe(
+        data=>{
+          console.log(data);
+        }
+      )
   }
 
 
@@ -65,6 +73,19 @@ export class DataComponent implements OnInit {
       return { passinvalid : true }
     }
     return true;
+  }
+
+  userexist( ctrl: FormControl ): Promise<any>|Observable<any>{
+    let promise = new Promise(
+      (resolve, reject)=>{
+        setTimeout(()=>{
+          if( ctrl.value === "negu" )
+            resolve( {exist:true} )
+          else
+            resolve( null )
+        }, 3000)
+      });
+      return promise;
   }
 
   newSkill() {
